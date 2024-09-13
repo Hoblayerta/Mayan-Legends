28cbc71e3337e68a4eb6f8b9b188e9d4f33d46bd
Hoblayerta <hoblayerta@gmail.com>
2024-09-13 03:29:42 -0600

    Initial commit

--- scripts/getNFTAddress.js
+++ scripts/getNFTAddress.js
@@ -1,6 +1,6 @@
 async function main() {
     // Usa la dirección del contrato MayanNFT que desplegaste
-    const nftAddress = "0x0b306bf915c4d645ff596e518faf3f9669b97016"; // Cambia esto si no es la dirección correcta
+    const nftAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Cambia esto si no es la dirección correcta
 
     // Obtén el contrato usando ethers.js
     const MayanNFT = await ethers.getContractFactory("MayanNFT");
